bed51901a8fd9c772c8102dbb0c3433b12e6469c
kyoncy <k.nagao@hitto.co.jp>
2022-08-18 19:55:04 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import styled from "@emotion/styled";
 
-function App() {
+const ignoreSsrWarning =
+  "/* emotion-disable-server-rendering-unsafe-selector-warning-please-do-not-use-this-the-warning-exists-for-a-reason */";
+
+const Container = styled.div(() => ({
+  display: "inline-block",
+  verticalAlign: "inherit",
+  "& > *": {
+    marginLeft: 0,
+    verticalAlign: "inherit"
+  },
+  [`& > *:first-child${ignoreSsrWarning}`]: {
+    marginLeft: 0
+  }
+}));
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Container />
     </div>
   );
 }
-
-export default App;
